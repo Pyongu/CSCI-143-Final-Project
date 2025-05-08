92bfe6cdfb1b8905638a6bd4334287a6954f8ca4
Pyongu <paulpkim845@gmail.com>
2025-05-08 16:14:20 -0700

    added tests

--- load_tweets.sh
+++ load_tweets.sh
@@ -11,8 +11,3 @@ echo 'load normalized'
 for file in $files; do
     python3 load_tweets.py --db "postgresql://postgres:pass@localhost:3334" --inputs "$file"
 done
-
-echo 'load denormalized'
-for file in $files; do
-     unzip -p "$file" | sed 's/\\u0000//g' | psql "postgresql://postgres:pass@localhost:3333" -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
-done
